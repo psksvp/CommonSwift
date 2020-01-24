6fa63495ba0d3e747c30954a29d1194933203bf2
psksvp <psksvp@gmail.com>
2020-01-24 23:08:31 +1100

    add javaScriptEscapedString from https://gist.github.com/pwightman/64c57076b89c5d7f8e8c

--- Sources/CommonSwift/Common.swift
+++ Sources/CommonSwift/Common.swift
@@ -99,7 +99,24 @@ public extension StringProtocol
 	subscript(range: CountablePartialRangeFrom<Int>) -> SubSequence { self[index(startIndex, offsetBy: range.lowerBound)...] }
 	subscript(range: PartialRangeThrough<Int>) -> SubSequence { self[...index(startIndex, offsetBy: range.upperBound)] }
 	subscript(range: PartialRangeUpTo<Int>) -> SubSequence { self[..<index(startIndex, offsetBy: range.upperBound)] }
+	
+	// mod to compile on swift 5,1 from https://gist.github.com/pwightman/64c57076b89c5d7f8e8c
+  func javaScriptEscapedString() -> String 
+	{
+    // Because JSON is not a subset of JavaScript, the LINE_SEPARATOR and PARAGRAPH_SEPARATOR unicode
+    // characters embedded in (valid) JSON will cause the webview's JavaScript parser to error. So we
+    // must encode them first. See here: http://timelessrepo.com/json-isnt-a-javascript-subset
+    // Also here: http://media.giphy.com/media/wloGlwOXKijy8/giphy.gif
+    let str = self.replacingOccurrences(of: "\u{2028}", with: "\\u2028")
+                  .replacingOccurrences(of: "\u{2029}", with: "\\u2029")
+    // Because escaping JavaScript is a non-trivial task (https://github.com/johnezang/JSONKit/blob/master/JSONKit.m#L1423)
+    // we proceed to hax instead:
+    let data = try! JSONSerialization.data(withJSONObject:[str], options: [])
+    let encodedString = NSString(data: data, encoding: String.Encoding.utf8.rawValue)!
+    return encodedString.substring(with: NSMakeRange(1, encodedString.length - 2))
+  }
 }
+
 
 //func synchronized(_ syncedObj: Any, block: () -> ())
 //{
