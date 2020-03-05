e04b0beb868b8c0518b115ad1473caad3e2de61d
psksvp <psksvp@gmail.com>
2020-03-05 20:46:42 +1100

    WkWebView extension

---
+++ Sources/CommonSwift/WkWebView+JSFunctionWrap.swift
@@ -0,0 +1,112 @@
+import Foundation
+import WebKit
+
+extension WKWebView
+{
+  func scrollToAnchor(_ s:String) -> Void
+  {
+    let anchor = "\"#\(s.lowercased().trim().replacingOccurrences(of: " ", with: "-"))\""
+    let js = "location.hash = \(anchor);"
+    //Log.info("about to eval javascript \(js)")
+    self.evaluateJavaScript(js)
+    {
+      (sender, error) in
+      dump(error)
+    }
+  }
+  
+  func scrollToParagrah(withSubString s: String, searchReverse: Bool = false) -> Void
+  {
+    let loopHead = searchReverse ? "for(var i = x.length - 1; i >= 0; i--)" :
+                                   "for(var i = 0; i < x.length; i++)"
+    let js = """
+    var bgColor = document.body.style.backgroundColor;
+    var x = document.querySelectorAll("p, q, li, h1, h2, h3");
+    var s = \(s)
+    \(loopHead)
+    {
+      if(x[i].textContent.indexOf(s) >= 0)
+      {
+        x[i].scrollIntoView({behavior: "smooth", block: "center", inline: "nearest"});
+        x[i].style.backgroundColor = "Azure";
+        break;
+      }
+      else
+      {
+        x[i].style.backgroundColor = bgColor;
+      }
+    }
+    """
+    //Log.info("about to eval javascript\n\(js)")
+    self.evaluateJavaScript(js)
+    {
+      (sender, error) in
+      if let _ = error
+      {
+        dump(error)
+      }
+    }
+  }
+  
+  // https://stackoverflow.com/questions/26851630/javascript-synchronous-native-communication-to-wkwebview
+  func syncEvaluateJavaScript(_ script: String) -> Any?
+  {
+    var result: Any?
+    var error: Error?
+    var done = false
+    let timeout = 3.0
+    if Thread.isMainThread
+    {
+      evaluateJavaScript(script)
+      {
+        (obj: Any?, err: Error?)->Void in
+        result = obj
+        error = err
+        done = true
+      }
+      while !done
+      {
+        let reason = CFRunLoopRunInMode(CFRunLoopMode.defaultMode, timeout, true)
+        if reason != CFRunLoopRunResult.handledSource
+        {
+            break
+        }
+      }
+    }
+    else
+    {
+        let condition: NSCondition = NSCondition()
+        DispatchQueue.main.async()
+        {
+          [weak self] in
+          self?.evaluateJavaScript(script)
+          {
+            (obj: Any?, err: Error?)->Void in
+            condition.lock()
+            result = obj
+            error = err
+            done = true
+            condition.signal()
+            condition.unlock()
+          }
+        }
+        condition.lock()
+        while !done
+        {
+          if !condition.wait(until: Date(timeIntervalSinceNow: timeout) as Date)
+          {
+            break
+          }
+        }
+        condition.unlock()
+    }
+    if error != nil { dump(error) }
+    if !done
+    {
+      Log.error("!Timeout to evaluate script: \(script)")
+    }
+    return result ?? nil
+  }
+  
+  
+}
